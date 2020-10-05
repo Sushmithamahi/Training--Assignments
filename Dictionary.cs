52d1b89e266d312c85e54742ad57fb941d658744
Sushmithamahi <72140762+Sushmithamahi@users.noreply.github.com>
2020-10-05 15:29:16 +0530

    Add files via upload

---
+++ Dictionary.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharpdictionary
+{
+    class Dictionary
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int,string> dictionary= new Dictionary<int, string>();
+            //add()to enter /insert to dictionary
+            dictionary.Add(1, "Bangalore");
+            dictionary.Add(2, "Mumbai");
+            dictionary.Add(3, "Pune");
+            dictionary.Add(4, "Hyderabad");
+            dictionary.Add(5, "chennai");
+           
+
+            foreach (KeyValuePair<int,string> check in dictionary) 
+            {
+                Console.WriteLine("elements of dict are: " +" "  + check.Key + check.Value);
+                
+            }
+            Console.WriteLine("no of ele:"+ dictionary.Count());
+            //set the item value like this also
+            dictionary[6] = "Delhi";
+            dictionary[7] = "Kolkata";
+            dictionary[8] = "Ahmedabad";
+            dictionary[9] = "Noida";
+
+            foreach (KeyValuePair<int, string> check in dictionary)
+            {
+                Console.WriteLine("elements of dict are:" + check.Key + check.Value);
+
+            }
+            Console.WriteLine("no of ele:" + dictionary.Count());
+
+            // remove() 2nd city
+            Console.WriteLine("ele left in the dict after removing 2nd city" + dictionary.Remove(2));
+            foreach (KeyValuePair<int, string> check in dictionary)
+            {
+                Console.WriteLine("elements of dict are:" + check.Key + check.Value);
+
+            }
+            Console.WriteLine("no of ele:" + dictionary.Count());
+
+
+            //removing 3rd city
+            Console.WriteLine("ele left in the dict after removing 3rd city" + dictionary.Remove(3));
+            foreach (KeyValuePair<int, string> check in dictionary)
+            {
+                Console.WriteLine("elements of dict are:" + check.Key + check.Value);
+
+            }
+            Console.WriteLine("no of ele:" + dictionary.Count());
+
+            //removing 4th city
+            Console.WriteLine("ele left in the dict after removing 4th city" + dictionary.Remove(4));
+            foreach (KeyValuePair<int, string> check in dictionary)
+            {
+                Console.WriteLine("elements of dict are:" + check.Key + check.Value);
+
+            }
+            Console.WriteLine("no of ele:" + dictionary.Count());
+
+            //removing 5th city
+            Console.WriteLine("ele left in the dict after removing 5th city" + dictionary.Remove(5));
+            foreach (KeyValuePair<int, string> check in dictionary)
+            {
+                Console.WriteLine("elements of dict are:" + check.Key + check.Value);
+
+            }
+            Console.WriteLine("no of ele:" + dictionary.Count());
+
+
+            //removing 6th city
+            Console.WriteLine("ele left in the dict after removing 6th city" + dictionary.Remove(6));
+            foreach (KeyValuePair<int, string> check in dictionary)
+            {
+                Console.WriteLine("elements of dict are:" + check.Key + check.Value);
+
+            }
+            Console.WriteLine("no of ele:" + dictionary.Count());
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Queue.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpQueue
+{
+    class Queue
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> Myqueue = new Queue<int>();// creation of queue object
+            Myqueue.Enqueue(2);
+            Myqueue.Enqueue(4);
+            Myqueue.Enqueue(6);
+            Myqueue.Enqueue(8);
+            Myqueue.Enqueue(10);
+            Myqueue.Enqueue(12);
+            Myqueue.Enqueue(14);
+            Myqueue.Enqueue(16);
+            Myqueue.Enqueue(18);
+
+            foreach(int DXC in Myqueue)
+            {
+                Console.WriteLine("queue elements are:" + DXC);
+            }
+            //one
+            Console.WriteLine("after dequeue:" + Myqueue.Dequeue());
+            Console.WriteLine("peek ele of dequeue:" + Myqueue.Peek());
+            Console.WriteLine("current ele in queue:" + Myqueue.Count());
+          
+            
+            Console.WriteLine("Myqueue contains:",Myqueue.Contains(10));
+
+            //2nd time
+            Console.WriteLine("after dequeue:" + Myqueue.Dequeue());
+
+            foreach (int DXC in Myqueue)
+            {
+                Console.WriteLine("queue elements are:" + DXC);
+            }
+            Console.WriteLine("peek ele of dequeue:" + Myqueue.Peek());
+            Console.WriteLine("current ele in queue:" + Myqueue.Count());
+
+            //3rd time
+            Console.WriteLine("after dequeue:" + Myqueue.Dequeue());
+
+            foreach (int DXC in Myqueue)
+            {
+                Console.WriteLine("queue elements are:" + DXC);
+            }
+            Console.WriteLine("peek ele of dequeue:" + Myqueue.Peek());
+            Console.WriteLine("current ele in queue:" + Myqueue.Count());
+
+            //4th time
+            Console.WriteLine("after dequeue:" + Myqueue.Dequeue());
+
+            foreach (int DXC in Myqueue)
+            {
+                Console.WriteLine("queue elements are:" + DXC);
+            }
+            Console.WriteLine("peek ele of dequeue:" + Myqueue.Peek());
+            Console.WriteLine("current ele in queue:" + Myqueue.Count());
+
+            //5th time
+            Console.WriteLine("after dequeue:" + Myqueue.Dequeue());
+
+            foreach (int DXC in Myqueue)
+            {
+                Console.WriteLine("queue elements are:" + DXC);
+            }
+            Console.WriteLine("peek ele of dequeue:" + Myqueue.Peek());
+            Console.WriteLine("current ele in queue:" + Myqueue.Count());
+
+            //6th time
+            Console.WriteLine("after dequeue:" + Myqueue.Dequeue());
+
+            foreach (int DXC in Myqueue)
+            {
+                Console.WriteLine("queue elements are:" + DXC);
+            }
+            Console.WriteLine("peek ele of dequeue:" + Myqueue.Peek());
+            Console.WriteLine("current ele in queue:" + Myqueue.Count());
+
+
+            Console.ReadLine();
+        }
+    }
+}
