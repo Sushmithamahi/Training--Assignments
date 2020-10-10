8bdd5d9bb7c83ccfc3d405c56074e0f30e860da5
Sushmithamahi <72140762+Sushmithamahi@users.noreply.github.com>
2020-10-10 13:04:29 +0530

    Add files via upload

---
+++ jointhread.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multithread.cs
+{
+    class jointhread
+    {
+        static void thread1()
+        {
+            for(int i=1;i<15;i++)
+            {
+                Console.WriteLine("thread1 is stred" + i);
+            }
+        }
+        static void Main(string[]args)
+        {
+            Console.WriteLine("main thread started");
+            thread t1 = new thread(thread1);
+            Console.ReadLine();
+        }
+
+    }
+}
---
+++ threadlock.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Deadlock
+{
+    class threadlock
+    {
+       
+            public void Display()
+            {
+               lock (this )//it will run both the threads as one 
+               {
+                Console.WriteLine("locks");
+                Thread.Sleep(5000);
+
+                Console.WriteLine("deadlocks");
+               }
+
+            }
+        
+       
+            
+        
+        static void Main(string[] args)
+        {
+                      
+            threadlock obj = new threadlock();
+            //instatiated the thread with method refernece
+            Thread t1 = new Thread(obj.Display);
+            Thread t2 = new Thread(obj.Display);
+            Thread t3 = new Thread(obj.Display);
+            t1.Start(); t2.Start();t3.Start();
+            Console.ReadLine();
+        }
+    }
+}
